bcb33e68ab9586ee03f57eaf2bdfba7fa2ad16ab
Jenny-D <jenny.duffka@uni-weimar.de>
2019-05-04 21:31:29 +0200

    Intervall [0, 1] versucht mit einzubringen

--- source/color.hpp
+++ source/color.hpp
@@ -5,6 +5,12 @@ struct Color{
 	float r = 0.5f;
 	float g = 0.5f;
 	float b = 0.5f;
+
+	/*Color(float r, float g, float b) :
+		if (r>1 || r<0 || b>1 || b<0 || g>1 || g<0) {
+			std::cout << "Eingabe muss zwischen 0 und 1 liegen!\n";
+			throw;
+	}*/
 };
 
 #endif
